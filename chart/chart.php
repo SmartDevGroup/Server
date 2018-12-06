b1da20739c75881f41798827bca85ab36f8c2d3b
GenriUolter <muhalenko.yaroslav@gmail.com>
2018-12-07 00:51:02 +0200

    fix chart

--- chart/chart.php
+++ chart/chart.php
@@ -6,8 +6,8 @@ date_default_timezone_set('Europe/Kiev');
 //$time."   ";
 //echo int mktime( 0, 0, 0, date("n"), date("j") - 1, date("Y"));
 
-//$lol = mktime (date("H"), date("i"), date("s") , date("n"), date("j") - 1, date("Y"))."  ";
-//$date = gmdate("Y-n-j G:i:s", $lol);
+$lol = mktime (date("H"), date("i"), date("s") , date("n"), date("j") - 1, date("Y"))."  ";
+$date = gmdate("Y-n-j G:i:s", $lol);
 
 $link=mysqli_connect("localhost", "root", "", "server");
 
@@ -21,8 +21,13 @@ while($row = mysqli_fetch_array($res))
   {
     if($row["b_sock_$i"] == "" || $row["b_sock_$i"] == 0){$j--;}
   }
-  $average = ($row['b_sock_1'] + $row['b_sock_2'] + $row['b_sock_3'] + $row['b_sock_4'] + $row['b_sock_5'] + $row['b_sock_6'])/$j;
-  $temp["b_temp_$a"] = $average;
+  if($j == 0){
+    $temp["b_temp_$a"] = 0;
+  }
+  else {
+    $average = ($row['b_sock_1'] + $row['b_sock_2'] + $row['b_sock_3'] + $row['b_sock_4'] + $row['b_sock_5'] + $row['b_sock_6'])/$j;
+    $temp["b_temp_$a"] = $average;
+  }
   $temp["date_$a"] = $row['date'];
   $a++;
   $b++;
@@ -31,9 +36,6 @@ while($row = mysqli_fetch_array($res))
 
 $dataPoints = array();
 for ($i=1; $i <= $b; $i++) {
-  //$timezone = 'Europe/Kiev';
-  //$date = new DateTime($temp["date_$i"], new DateTimeZone($timezone));
-  //$timestamp = $date->format('U');
   array_push($dataPoints, array("x" => strtotime($temp["date_$i"]) * 1000, "y" => $temp["b_temp_$i"]));
 }
 $json = json_encode($dataPoints);
--- service/raspi_send.php
+++ service/raspi_send.php
@@ -14,7 +14,7 @@ for ($i=1; $i < 13; $i++) {
 ob_start();
 var_dump($array);
 $output = ob_get_clean();
-file_put_contents('log.txt', $output." ".$array["temp_1"]." ".$array["conn_1"]);
+file_put_contents('log.txt', $output);
 
 $update_bedroom = mysqli_query($link, "UPDATE bedroom SET b_temp_sock_1 = $array[temp_1], b_temp_sock_2 = $array[temp_2], b_temp_sock_3 = $array[temp_3], b_temp_sock_4 = $array[temp_4], b_temp_sock_5 = $array[temp_5], b_temp_sock_6 = $array[temp_6], con_sock_1 = $array[conn_1], con_sock_2 = $array[conn_2],
   con_sock_3 = $array[conn_3], con_sock_4 = $array[conn_4], con_sock_5 = $array[conn_5], con_sock_6 = $array[conn_6], date= NOW() WHERE api_key='$get_key'");
