9233c0c9dea6d79adc12e202895d078080ab7fc2
GenriUolter <muhalenko.yaroslav@gmail.com>
2019-01-12 14:20:17 +0200

    fix log

--- service/send_mob.php
+++ service/send_mob.php
@@ -6,9 +6,45 @@ for ($i=1; $i < 9; $i++) {
     $array["value_$i"] = $data["$i"]["value"];
     $array["room_$i"] = $data["$i"]["room"];
 }
-ob_start();
-var_dump($array);
-$output = ob_get_clean();
-file_put_contents('/OSPanel/domains/log/log_mob.txt', $get_key." ".$output);
+//LOG SAVE
+$log = "../log/";
+if (!is_dir($log))
+{
+  mkdir($log);
+}
+else {
+  ob_start();
+  $name = date("G-i-s j-n-Y");
+  $user_dir = "../log/$get_key/";
+  if (!is_dir($user_dir)){
+    mkdir($user_dir);
+  }
+  $dir_save = $user_dir."mob/";
+  $count = count(scandir($dir_save));
+  $file = $count."_".$name.".txt";
+  if (!is_dir($dir_save))
+  {
+    mkdir($dir_save);
+    var_dump($array);
+    $output = ob_get_clean();
+    file_put_contents($dir_save.$file, $output);
+  }
+  else {
+    var_dump($array);
+    $output = ob_get_clean();
+    file_put_contents($dir_save.$file, $output);
+  }
+
+  if($count > 20)
+  {
+    foreach (new DirectoryIterator($dir_save) as $fileInfo) {
+      if(!$fileInfo->isDot()) {
+          unlink($fileInfo->getPathname());
+      }
+    }
+    exit;
+  }
+}
+//!LOG SAVE
 
 ?>
